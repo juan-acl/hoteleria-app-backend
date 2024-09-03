6ab9724ba28b1e9a453a04c7e03723c915b998a9
Juan Chuc <dev.juan.eloper@gmail.com>
2024-09-02 20:56:33 -0600

    Initial project with spring boot

---
+++ src/test/java/com/hoteleria_app/hoteleria/HoteleriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoteleria_app.hoteleria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteleriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
